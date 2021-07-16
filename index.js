e25646931aad11ed80dc440bc45bcdfb5b5be3f8
Jonathan Montes Romero <jmontes8686@gmail.com>
2021-07-15 22:09:07 -0700

    Made progress with changing to the next screen with the first question and tried figuring out how to make question selection work

--- index.js
+++ index.js
@@ -1,84 +1,94 @@
-const startButton = document.querySelector('#start-Bttn')
+const startButton = document.querySelector('#start-Bttn');
 // Section for questions
-const questionSection = documennt.querySelector('#questions')
+let questionSection = document.querySelector('#questions');
 
 let firstPageStart = document.querySelector('#start-page');
 let timeEl = document.querySelector('#timer');
-let scoreEl = document.querySelector('#scores')
-let quizTimer = document.querySelector('#timer')
-let quizBody = document.querySelector('#quiz')
-let scoreNameInput = document.querySelector('#nameForScore')
+let scoreEl = document.querySelector('#scores');
+let quizTimer = document.querySelector('#timer');
+let quizBody = document.querySelector('#quiz');
+let scoreNameInput = document.querySelector('#nameForScore');
+let userFinalScore = document.querySelector('#your-Score');
 
-let questionsAnswered = 0;
+let questionBeingAnswered = 0;
 let timeLeft = 80;
+let timerInterval;
+let score = 0;
+let itsCorrect;
 
-let buttonA = document.querySelector('#a')
-let buttonB = document.querySelector('#b')
-let buttonC = document.querySelector('#c')
-let buttonD  = document.querySelector('#d')
+let buttonA = document.querySelector('#a');
+let buttonB = document.querySelector('#b');
+let buttonC = document.querySelector('#c');
+let buttonD  = document.querySelector('#d');
 
 
-const questionBank = [
+let questionBank = [
   // Q,C,and  A for question 0
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'a. answer'
   },
 
   // Q,C,and A for question 1
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'b. answer'
   },
 
   // Q,C,and A for question 2
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'a. answer'
   },
 
   // Q,C,and A for question 3
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'a. answer'
   },
   
   // Q,C,and A for question 4
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'a. answer'
   },
 
   // Q,C,and A for question 5
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'a. answer'
   },
 
   // Q,C,and A for question 6
   {
     question: 'What is ... ?',
     choices: ['a. answer', 'b. answer', 'c. answer', 'd. answer'],
-    correctAnswer: 'a'
+    correctAnswer: 'a. answer'
   },
-]
+];
+
+let everyQuestionUsed = questionBank.length;
 
 function generateQuestions() {
-  if (questionsAnswered === questionBank.length) {
-    return finalScore
+
+  if (questionBeingAnswered === everyQuestionUsed) {
+    return finalScore();
   }
-  var currentQuestion = questionBank[questionsAnswered];
+  var currentQuestion = questionBank[questionBeingAnswered];
   questionSection.innerHTML = '<p>' + currentQuestion.questionBank + '<p>';
+  buttonA.innerHTML = currentQuestion.choices[0];
+  buttonB.innerHTML = currentQuestion.choices[1];
+  buttonC.innerHTML = currentQuestion.choices[2];
+  buttonD.innerHTML = currentQuestion.choices[3];
+};
 
-}
-
-function startQuiz() { {
+function startQuiz(){
   firstPageStart.style.display = 'none';
   generateQuestions();
 
@@ -86,21 +96,39 @@ function startQuiz() { {
   timerInterval = setInterval(function() {
     timeLeft--;
     quizTimer.textContent = 'Time Left: ' + timeLeft;
-  },
-    if (timeLeft === 0) {
-      clearInterval(timerInterval)
-      showScore();
-      // showscore();
-    }
-},
+                                                     
+  if (timeLeft === 0) { 
+    clearInterval(timerInterval);
+    finalScore();
+  }
+ }, 1000);
 
-  function showScore() {
+// function checkAnswer(answer){
+//   itsCorrect = questionBank[questionBeingAnswered].correctAnswer;
+
+//   if (answer === itsCorrect && questionBeingAnswered != everyQuestionUsed){
+//     score++;
+//     alert('That is Correct!');
+//     questionBeingAnswered++;
+//     generateQuestions();
+  
+//   } else if(answer !== itsCorrect && questionBeingAnswered !== everyQuestionUsed) {
+//     alert('That is incorrect.')
+//     currentQuestion++;
+//     generateQuestions();
+  
+//   } else {
+//     finalScore();
+//   }
+// }
+
+
+function finalScore() {
     quizBody.style.display = 'none';
     clearInterval(timerInterval);
     scoreNameInput.value = '';
+    userFinalScore.innerHTML = 'You got ' + score + ' out of ' + questionSection.length + ' correct!';
+}
+};
 
-
-  }
-
-
- startButton.addEventListener('click', startQuiz)
+startButton.addEventListener('click', startQuiz);
